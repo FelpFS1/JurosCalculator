c85e252083bc323a5be8f098ca151a82c8212d5f
FelpFS1 <phff800@gmail.com>
2022-05-14 12:40:56 -0300

    :tada: first commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# JurosCalculator
+
+
+# Ícones - Icons
+:boom: Atualização / Update
+:bug: Resolução de bug / Bugfix
+:sparkles: Nova funcionalidade / New feature
+:green_heart: Release
+
+# Sobre - About
+
+## pt-BR
+
+- Um projeto simples de uma calculadora de juros, feita com intuito de treinar logica e buscar mais conhecimento!
+
+## English
+
+- A simple project of an interest calculator, made in order to train logic and seek more knowledge!
+
+# O que foi usado? - what was used?
+## pt-BR
+### Bem,aqui foi usado algumas linguagens como:
+
+- HTML
+- CSS
+- JAVASCRIPT
+## English
+#### Well, here we used some languages ​​like:
+
+- HTML
+- CSS
+- JAVASCRIPT
+## Créditos- Credits
+<a href="https://www.linkedin.com/in/felipe-farias-891073229/"> Felipe Farias </a>
---
+++ main.js
@@ -0,0 +1,64 @@
+let inicial = document.querySelector('#inicial');
+let percentage = document.querySelector('#percentage')
+let time = document.querySelector('#time-course')
+let inputs = document.querySelector('.table')
+
+
+
+
+inicial.addEventListener("change", function(){
+    this.value = parseFloat(this.value).toFixed(2);
+ });
+
+ percentage.addEventListener("change", function(){
+    this.value = parseFloat(this.value).toFixed(2);
+ });
+
+function calcular() {
+
+    valueInicial = parseFloat(inicial.value)
+    valuePercentage = parseFloat(percentage.value)
+    valueTime = parseInt(time.value)
+   
+     if(inicial.value.length < 1 && percentage.value.length < 1 && time.value.length < 1 ){
+        alert("Preencha todos os campos")
+     }else{
+    inputs.style.border="5px solid #abb0c1"
+     }
+    for (let i = 0; i < valueTime; i++) {
+        let porcento = valueInicial * (valuePercentage / 100)
+        console.log(porcento);
+        valueInicial = valueInicial + porcento
+        console.log(valueInicial);
+        let y = document.createElement('h3')
+        let x = document.createElement('input')
+        x.readOnly = true
+        inputs.appendChild(y).innerHTML = ` MES ${i + 1}`
+        inputs.appendChild(x)
+        x.value = valueInicial.toFixed(2)
+
+    }
+
+    
+}
+
+
+document.getElementById("btn-clear").addEventListener('click',()=>{
+    inicial.value = ""
+    percentage.value = ""
+    time.value= ""
+    document.getElementById('table').remove()
+    location.reload()
+
+    
+})
+
+
+
+
+
+
+
+
+
+
